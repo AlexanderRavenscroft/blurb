120666accd1c22995ea455d9d7a58c267de9ff70
Alexander Ravenscroft <alex.ravenscroft.dev@gmail.com>
2026-09-07 12:33:30 +0200

    feat: gap package add

---
+++ lib/theme/app_radius.dart
@@ -0,0 +1,10 @@
+abstract final class AppRadius {
+  static const double none = 0;
+  static const double xs = 4;
+  static const double sm = 8;
+  static const double md = 12;
+  static const double lg = 16;
+  static const double xl = 24;
+  static const double xxl = 32;
+  static const double full = 999;
+}
